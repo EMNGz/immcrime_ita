79b001315cfb71bcf80aeb4fd0bbe4679c4690a7
EMNGz <gameball326@gmail.com>
2024-01-25 15:15:59 +0700

    ข้อความคอมมิต

--- test.php
+++ test.php
@@ -22,58 +22,103 @@
   <div id="accordion" class="col-md-12">
   <div class="white-box  mx-auto">
     <div class="card ">
-      <div class="card-header" id="headingOne">
+      <div class="card-header" id="line1"> <!-- บรรทัดที่ 1  -->
         <h5 class="mb-0">
        <button class="btn btn-link custom-btn d-flex justify-content-between" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
     ตัวชี้วัดย่อยที่ 9.1 ข้อมูลพื้นฐาน <i class="bi bi-plus-circle-fill icon-right text-primary "></i>
 </button>
         </h5>
       </div>
-      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-bs-parent="#accordion">
+      <div id="collapseOne" class="collapse show" aria-labelledby="line1" data-bs-parent="#accordion">
         <div class="card-body">
-        <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
-            <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 02 อำนาจหน้าที่และพื้นที่รับผิดชอบ</p>
-            <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 03 กฎหมายที่เกี่ยวข้อง</p>
-            <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 04 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 02 อำนาจหน้าที่และพื้นที่รับผิดชอบ</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 03 กฎหมายที่เกี่ยวข้อง</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 04 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
         </div>
       </div>
     </div>
     <div class="card">
-      <div class="card-header" id="headingTwo">
+      <div class="card-header" id="line2"> <!-- บรรทัดที่ 2  -->
         <h5 class="mb-0">
         <button class="btn btn-link custom-btn d-flex justify-content-between" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
         ตัวชี้วัดย่อยที่ 9.2 การบริหารงาน <i class="bi bi-plus-circle-fill icon-right text-primary  " ></i>
 </button>
         </h5>
       </div>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-bs-parent="#accordion">
+      <div id="collapseTwo" class="collapse" aria-labelledby="line2" data-bs-parent="#accordion">
         <div class="card-body">
-            <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
-            <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 04 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 04 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
         </div>
       </div>
     </div>
     <div class="card">
-      <div class="card-header" id="headingThree">
+      <div class="card-header" id="line3"> <!-- บรรทัดที่ 3  -->
         <h5 class="mb-0">
           <button class="btn btn-link collapsed custom-btn d-flex justify-content-between" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          ตัวชี้วัดย่อยที่ 9.3 การบริหารงาน <i class="bi bi-plus-circle-fill icon-right text-primary"></i>
+          ตัวชี้วัดย่อยที่ 9.3 การบริหารเงินงบประมาณ <i class="bi bi-plus-circle-fill icon-right text-primary"></i>
 </button>
-          
         </h5>
       </div>
-      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-bs-parent="#accordion">
+      <div id="collapseThree" class="collapse" aria-labelledby="line3" data-bs-parent="#accordion">
         <div class="card-body">
-            <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
-            <p class="custom-text"><i class="bi bi-circle-fill green-circle"></i> 02 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 02 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
         </div>
       </div>
     </div>
-  
+    
+    <div class="card">
+      <div class="card-header" id="line4"><!-- บรรทัดที่ 4  -->
+        <h5 class="mb-0">
+        <button class="btn btn-link custom-btn d-flex justify-content-between" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+        ตัวชี้วัดย่อยที่ 9.4 การบริหารและพัฒนาทรัพยากรบุคคล <i class="bi bi-plus-circle-fill icon-right text-primary  " ></i>
+</button>
+        </h5>
+      </div>
+      <div id="collapseFour" class="collapse" aria-labelledby="line4" data-bs-parent="#accordion">
+        <div class="card-body">
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 04 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header" id="line5"> <!-- บรรทัดที่ 5  -->
+        <h5 class="mb-0">
+        <button class="btn btn-link custom-btn d-flex justify-content-between" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+        ตัวชี้วัดย่อยที่ 10.1 การดำเนินการเพื่อป้องกันการทุจริต <i class="bi bi-plus-circle-fill icon-right text-primary  " ></i>
+</button>
+        </h5>
+      </div>
+      <div id="collapseFive" class="collapse" aria-labelledby="line5" data-bs-parent="#accordion">
+        <div class="card-body">
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 04 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header" id="line6"> <!-- บรรทัดที่ 6  -->
+        <h5 class="mb-0">
+        <button class="btn btn-link custom-btn d-flex justify-content-between" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+       ตัวชี้วัดย่อยที่ 10.2 มาตรการภายในเพื่อส่งเสริมคุณธรรมและความโปร่งใส <i class="bi bi-plus-circle-fill icon-right text-primary  " ></i>
+</button>
+        </h5>
+      </div>
+      <div id="collapseSix" class="collapse" aria-labelledby="line6" data-bs-parent="#accordion">
+        <div class="card-body">
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 01 โครงสร้าง อัตรากำลัง และข้อมูลผู้บริหาร</p>
+        <p class="custom-text d-flex align-items-center"> <i class="bi bi-circle-fill green-circle small-icon" ></i> 04 ข้อมูลการติดต่อ และช่องทาง การถาม-ตอบ/รับฟังความคิดเห็น</p>
+        </div>
+      </div>
+    </div>
+    </div>
   </div>
-    </div></div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
+  </script>
 </body>
 
 </html>
